a48121bfb6867779104a0d5a15ea38a9f41fe9b6
ErDoggi <155678572+ErDoggi@users.noreply.github.com>
2025-11-20 00:09:16 +0100

    nuevos  archivos

---
+++ src/app/pages/ch2-2/ch2-2.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ch2-2',
+  imports: [],
+  templateUrl: './ch2-2.html',
+})
+export class Ch22 {
+
+}
---
+++ src/app/pages/ch2-3/ch2-3.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ch2-3',
+  imports: [],
+  templateUrl: './ch2-3.html',
+})
+export class Ch23 {
+
+}
